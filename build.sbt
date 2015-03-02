ddf1e4a4a9c01ec96f0e9bb84134038a512f36a7
James Murphy <jmurphy84@googlemail.com>
2015-03-02 21:18:05 +0000

    Added skeleton to start Scala Tests for Board class

--- build.sbt
+++ build.sbt
@@ -4,3 +4,5 @@ lazy val root = (project in file(".")).
       version := "1.0",
       scalaVersion := "2.11.5"
   )
+
+libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.2" % "test"
---
+++ src/test/scala/BoardSpec.scala
@@ -0,0 +1,9 @@
+import collection.mutable.Stack
+import org.scalatest._
+
+class BoardSpec extends FlatSpec with Matchers {
+
+    "A Board" should "be initialised with an empty state of 64-bit integers" in {
+        
+    }
+}
